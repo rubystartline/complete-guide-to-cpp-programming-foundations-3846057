757f1ac8e6cfe503e9f48b436678b0df738475d4
Jinel <jinelb12@gmail.com>
2025-06-17 09:14:22 +0000

    updated as of june 17

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -6,16 +6,17 @@
 #include <cstdint>
 
 int add_int(float a, double b, long double c){
+    
     int result = 0;
 
-    // Write your code here
+    result = static_cast<int>(a) + static_cast<int>(b) + static_cast<int>(c); 
     
     return result;
 }
 
 int main(){
-    float a = 2.1;
-    double b = 3.9;
+    float a = -2.0;
+    double b = -3.9;
     long double c = 4.6;
 
     int learnerResult = add_int(a, b, c);
